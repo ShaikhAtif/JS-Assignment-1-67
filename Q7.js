73d0623a87762fc8eefe7cfd374a2c2568b3ded3
Muhammad Atif <64535636+ShaikhAtif@users.noreply.github.com>
2020-07-24 19:53:28 +0500

    Delete Q7.js

--- Q7.js
+++
@@ -1,8 +0,0 @@
-var a=["SSC","HSC","BCS","BS","BCOM","MS","M.Phil.","PhD"];
-
-console.log("Qualification :")
-for (let index = 0; index < a.length; index++) {
-    
-console.log(index+1,')' ,a[index]);
-    
-}
