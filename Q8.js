a6c4bada4fa83c31941a5b5a846b03f8d9e2051c
Muhammad Atif <64535636+ShaikhAtif@users.noreply.github.com>
2020-07-24 19:53:29 +0500

    Delete Q8.js

--- Q8.js
+++
@@ -1,6 +0,0 @@
-var score = [500, 453, 408];
-var name = ["Atif", "Hamza", "Ahmed"];
-for (let index = 0; index < 3; index++) {
-    console.log("Score of ",name[index]," is ",score[index],".Percentage: ",score[index]/500*100,"%");    
-    
-}
